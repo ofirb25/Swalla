19de3656cbf26c118ca0644d833ab98a221485ff
Avinoam Alaluf <avinoamalaluf@gmail.com>
2017-12-25 17:30:09 +0200

    added admin mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HomePage from '../pages/HomePage'
+import MyGamesPage from '../pages/MyGamesPage'
 
 Vue.use(Router)
 
@@ -11,6 +12,12 @@ export default new Router({
       path: '/',
       name: 'Home-Page',
       component: HomePage
+    },
+    {
+      path: '/MyGames',
+      name: 'My-Games-Page',
+      component: MyGamesPage
     }
+
   ]
 })
