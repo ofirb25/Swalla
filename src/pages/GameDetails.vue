cbb4842f59d67ab6c4438dcb1ff73144b7cd15e4
ofir mossinson <ofirmoss@gmail.com>
2017-12-27 11:36:21 +0200

    pigow

--- src/pages/GameDetails.vue
+++ src/pages/GameDetails.vue
@@ -57,31 +57,13 @@
       </v-btn>
     </div>
 
-     <v-dialog v-model="dialog" max-width="500">
-      <v-card>
-        <v-card-title class="headline">Choose game style:</v-card-title>
-        <v-card-text class="modal-content">
-          <router-link tag="div" :to="'/game/'+ game._id+'/play'">
-          <div>
-            <img src="../assets/single-player.png">
-            <h4>Single player</h4>
-          </div>
-          </router-link>
-          <h1>or</h1>
-          <div>
-            <img src="../assets/multiple-users.png">
-             <h4>Invite players</h4>
-          </div>
-          </v-card-text>
-        
-      </v-card>
-    </v-dialog>
-
-
+<startgame-modal :dialog="dialog" :game="game"></startgame-modal>
+  
   </section>
 </template>
 <script>
   import GameService from "../services/GamesService";
+  import StartgameModal from "../components/EditCmps/StartGameModal.vue"
   export default {
     data() {
       return {
@@ -94,6 +76,9 @@
       GameService.getGameById(gameId).then(game => {
         this.game = game;
       });
+    },
+    components:{
+      StartgameModal
     }
   }
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,24 +1,54 @@
 <template>
     <section class="games-section">
         <div class="container">
+            <h1>READY TO DO PIGUIM?</h1>
+        <div class="input-container">
+        <input style="text" placeholder="⌕ Search game"/>
+        <input style="text" placeholder="Enter your pin here"/>
+        </div>
         <game-list></game-list>
         </div>
     </section>
 </template>
 
 <script>
-import GameList from '../components/HomeComps/GameList'
+import GameList from "../components/HomeComps/GameList";
 export default {
-    components : {
-        GameList
-    }
-}
+  components: {
+    GameList
+  }
+};
 </script>
 
-<style>
-.games-section{
-    display:flex;
-    justify-content: center;
+<style lang="scss" scoped>
+.games-section {
+  display: flex;
+  justify-content: center;
+
+  .container {
+    align-self: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1{
+         margin-bottom: 10px;
+         font-size: 70px;
+         color: rgba(176, 132, 226, 0.699);
+    }
+
+    .input-container {
+        width: 50%;
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 10px;
+      input {
+        border: 2px solid rgba(0, 0, 0, 0.452);
+        border-radius: 3px;
+        width: 48%;
+      }
+    }
+  }
 }
 </style>
 
