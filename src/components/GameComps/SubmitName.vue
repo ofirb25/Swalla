ef3b89c276c944341e651930fa639c3bb9ebbfe8
OfirBlumshtein <ofzir11@gmail.com>
2017-12-28 14:54:45 +0200

    added start game

--- src/components/GameComps/SubmitName.vue
+++ src/components/GameComps/SubmitName.vue
@@ -3,7 +3,7 @@
         <v-form @submit.prevent="saveName">
             <v-text-field label="Your Nickname??" v-model="playerName" :counter="10">
             </v-text-field>
-            <v-btn color="purple" @click="saveName">Start Playing!</v-btn>
+            <v-btn @click="saveName">Start Playing!</v-btn>
         </v-form>
     </div>
 </template>
@@ -22,6 +22,12 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+form{
+    background:rgba(255, 255, 255, 0.467);
+    box-shadow: 3px 3px 3px rgba(85, 84, 84, 0.192);
+    padding:150px;
+    color:white;
+}
 .nick {
   height: 90vh;
   display: flex;
