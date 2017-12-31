64c45661a09922ee0c1071d829a70b8f4a3e1192
ofir mossinson <ofirmoss@gmail.com>
2017-12-31 12:23:06 +0200

    pigow

--- src/components/GameComps/ScoreBoard.vue
+++ src/components/GameComps/ScoreBoard.vue
@@ -1,12 +1,18 @@
 <template>
-    <div v-if="players">
-    <h1 class="display-2">Score Board</h1>
-    <div class="userScore" v-for="player in players" :key="player.userId">
-    {{player.nickname}} -
-    {{player.score}}
+    <div v-if="players" class="score-board">
+    <h1 class="display-2 board-title">Score Board</h1>
+    <div class="userScore" v-for="(player,idx) in players" :key="idx">
+      <div>
+       {{idx+1}}. {{player.nickname}}:
+      </div>
+     <div>
+       {{player.score}}
+       <img src="../../assets/coins.png">
+     </div>
     </div>
     </div>
 </template>
+
 <script>
 export default {
   props : {
@@ -14,3 +20,32 @@ export default {
   },
 };
 </script>
+
+ <style lang="scss" scoped>
+.score-board {
+  height: 100%;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  h1 {
+    margin-bottom: 20px;
+  }
+
+  .userScore {
+    font-size: 30px;
+    width: 20%;
+    display: flex;
+    justify-content: space-around;
+
+    img {
+      width: 17px;
+    }
+  }
+
+  .board-title {
+    font-weight: bold;
+  }
+}
+</style>
